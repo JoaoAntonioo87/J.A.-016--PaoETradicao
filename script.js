d28f57732374b785d4bc478cdc739ab7e4e4e7b4
João Antonio <contatojoao87@gmail.com>
2026-01-28 20:33:12 -0300

    Criado menu hamburgue, fazer o JS do dropdown

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// HEADER MENU
 
+const navHeader = Array.from(
+  document.getElementsByClassName("header-nav-links"),
+);
+
+navHeader.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    console.log("funcionou");
+  });
+});
+
+console.log(navHeader);
