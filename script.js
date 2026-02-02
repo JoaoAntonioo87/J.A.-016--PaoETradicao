8da67cc64995fb91294384991ffab81c3f7ce197
João Antonio <contatojoao87@gmail.com>
2026-02-02 20:12:05 -0300

    Header responsivo

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const navHeader = Array.from(
   document.getElementsByClassName("header-nav-links"),
 );
 
-navHeader.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    element.classList.toggle("active");
-    event.stopPropagation();
-  });
+if (window.screen.availWidth < 1000) {
+  navHeader.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      element.classList.toggle("active");
+      event.stopPropagation();
+    });
 
-  window.addEventListener("click", (event) => {
-    if (Array.from(navHeader[0].classList).includes("active")) {
-      navHeader[0].classList.remove("active");
-    }
+    window.addEventListener("click", (event) => {
+      if (Array.from(navHeader[0].classList).includes("active")) {
+        navHeader[0].classList.remove("active");
+      }
+    });
   });
-});
+}
