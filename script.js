aaee67f7b8f5f4e1950928616106961e81f2ecb5
João Antonio <contatojoao87@gmail.com>
2026-02-03 20:32:47 -0300

    Início da introdução da home, já feito JS para imagem passando automáticamente

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ if (window.screen.availWidth < 1000) {
     });
   });
 }
+
+// IMAGE INTRO
+
+const imgIntro = document.getElementsByClassName("intro-img");
+let i = 1;
+
+setInterval(() => {
+  if (i === 8) {
+    imgIntro[0].src = "img/img-01.jpg";
+    i = 1;
+  } else {
+    imgIntro[0].src = `img/img-0${i + 1}.jpg`;
+    i = i + 1;
+  }
+  console.log(i);
+}, 5000);
